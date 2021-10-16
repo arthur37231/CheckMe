a364ccdb86531587b2966222a937c7cdfdc1ae76
zmia3786 <zmia3786@uni.sydney.edu.au>
2021-10-17 02:52:49 +0800

    Implemented navigation bar and view pagers

--- app/src/main/java/comp5216/sydney/edu/au/checkme/view/NavigationBar.java
+++ app/src/main/java/comp5216/sydney/edu/au/checkme/view/NavigationBar.java
@@ -3,65 +3,59 @@ package comp5216.sydney.edu.au.checkme.view;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import comp5216.sydney.edu.au.checkme.R;
+import comp5216.sydney.edu.au.checkme.activity.MainActivity;
 
-public class NavigationBar extends LinearLayout {
-    private Button home;
-    private Button myCode;
-    private Button scan;
-    private Button history;
-    private Button account;
-    private BottomNavigationView bottomNavigationView;
+public class NavigationBar extends LinearLayout{
+    private final BottomNavigationView bottomNavigationView;
+    private MainActivity fragmentHolder;
 
     public NavigationBar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        LayoutInflater.from(getContext()).inflate(R.layout.navigation_bar, this);
+        LayoutInflater.from(context).inflate(R.layout.navigation_bar, this);
 
         this.bottomNavigationView = findViewById(R.id.bottomNavBar);
-
-//        this.home = findViewById(R.id.home);
-//        this.myCode = findViewById(R.id.myCode);
-//        this.scan = findViewById(R.id.scan);
-//        this.history = findViewById(R.id.history);
-//        this.account = findViewById(R.id.account);
-    }
-
-    public NavigationBar setupHome(OnClickListener listener) {
-        this.home.setOnClickListener(listener);
-        return this;
-    }
-
-    public NavigationBar setupMyCode(OnClickListener listener) {
-        this.myCode.setOnClickListener(listener);
-        return this;
-    }
-
-    public NavigationBar setupScan(OnClickListener listener) {
-        this.scan.setOnClickListener(listener);
-        return this;
-    }
-
-    public NavigationBar setupHistory(OnClickListener listener) {
-        this.history.setOnClickListener(listener);
-        return this;
-    }
-
-    public NavigationBar setupAccount(OnClickListener listener) {
-        this.account.setOnClickListener(listener);
-        return this;
+        this.bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    fragmentHolder.navigateTo(0);
+                    break;
+                case R.id.myCode:
+                    fragmentHolder.navigateTo(1);
+                    break;
+                case R.id.scan:
+                    fragmentHolder.navigateTo(2);
+                    break;
+                case R.id.history:
+                    fragmentHolder.navigateTo(3);
+                    break;
+                case R.id.account:
+                    fragmentHolder.navigateTo(4);
+                    break;
+            }
+            return true;
+        });
     }
 
     public void setInvisible() {
         this.setVisibility(View.GONE);
+    }
+
+    public void navigateTo(int position) {
+        this.bottomNavigationView.getMenu().getItem(position).setChecked(true);
+    }
+
+    public void setFragmentHolder(MainActivity fragmentHolder) {
+        this.fragmentHolder = fragmentHolder;
     }
 }
