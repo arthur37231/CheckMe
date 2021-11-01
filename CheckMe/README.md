ad4b9e33b76b5d7d08a9359303b798bf2e3350b1
zmia3786 <zmia3786@uni.sydney.edu.au>
2021-11-01 15:29:26 +0800

    Removing fucking shit code

--- CheckMe/README.md
+++
@@ -1,2 +0,0 @@
-# CheckMe
-2021 Semester 2 COMP5216 Group 14 Project
--- CheckMe/app/src/main/java/comp5216/sydney/edu/au/checkme/utils/CommonUtils.java
+++
@@ -1,7 +0,0 @@
-package comp5216.sydney.edu.au.checkme.utils;
-
-public class CommonUtils {
-    public static String phoneNumberToEmail(String phoneNumber) {
-        return phoneNumber.substring(5).trim() + "@checkme.com";
-    }
-}
