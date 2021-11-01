6d933be792cfe1466011c1c1d4f1eb71dd7ac397
fangpeich <35284309+fangpeich@users.noreply.github.com>
2021-11-01 11:57:09 +1100

    update ui and event compare to

--- CheckMe/app/src/main/java/comp5216/sydney/edu/au/checkme/activity/MapActivity.java
+++ CheckMe/app/src/main/java/comp5216/sydney/edu/au/checkme/activity/MapActivity.java
@@ -27,13 +27,6 @@ import comp5216.sydney.edu.au.checkme.R;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
-    private GoogleMap mMap;
-
-
-    Button btn;
-    private final static int PLACE_PICKER_REQUEST = 999;
-    private final static int LOCATION_REQUEST_CODE = 23;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,22 +45,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-33.867, 151.206);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         map.setMyLocationEnabled(true);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
 
-        map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
+        map.addMarker(new MarkerOptions().title("Sydney").position(sydney));
         map.getUiSettings().setZoomControlsEnabled(true);
         map.setOnMapLongClickListener(this);
 
@@ -81,26 +64,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapLongClick(LatLng latLng) {
 
-        Geocoder geocoder;
-        List<Address> addresses;
-        geocoder = new Geocoder(this.getApplicationContext(), Locale.getDefault());
-
-        try {
-            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-            String city = addresses.get(0).getLocality();
-            String state = addresses.get(0).getAdminArea();
-            String country = addresses.get(0).getCountryName();
-            String postalCode = addresses.get(0).getPostalCode();
-            String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
-            Toast.makeText(this,address+" "+city,Toast.LENGTH_SHORT).show();
-            Intent data = new Intent();
-            data.putExtra("Latlng", latLng);
-            setResult(RESULT_OK, data);
-            finish();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Intent data = new Intent();
+        data.putExtra("Latlng", latLng);
+        setResult(RESULT_OK, data);
+        finish();
 
     }
 }
